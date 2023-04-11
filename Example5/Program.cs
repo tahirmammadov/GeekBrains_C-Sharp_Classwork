cd21a479727bc92e21e4bfff0df1fd88869b70c9
Tahir Mammadov <tmammadovt@gmail.com>
2023-04-11 15:27:05 +0400

    added 5-7

---
+++ Example5/Program.cs
@@ -0,0 +1,42 @@
+﻿//  Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Clear();
+
+Console.Write($"Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n <= 0)
+{
+    Console.Write($"\nНеправильное значение!\nВведите значение больше 0: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+//1-st option (loop)
+Console.Write("Результат: ");
+for (int i = -n; i <= n; i++)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+
+//2-nd option (recursion)
+// void Numbers(int n, int i)
+// {
+//     if (n >= 0)
+//     {
+//         Console.Write($"{n} ");
+//         i++;
+//         Numbers(n - 1, i);
+//     }
+
+//     if (n < 0 && i > 1)
+//     {
+//         Console.Write($"{n} ");
+//         i--;
+//         Numbers(n - 1, i);
+//     }
+
+// }
+
+// Numbers(n, 0);
+// Console.WriteLine();
---
+++ Example6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Clear();
+
+Console.Write($"Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while((num<100 || num>999) && (num>-100 || num <-999))
+{
+    Console.Write($"Не верное значение!\nВведите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Результат: {num%10}");
