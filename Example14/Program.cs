13301db5076a3e72413eb6742d76298d918d9107
Tahir Mammadov <tmammadovt@gmail.com>
2023-04-13 17:59:14 +0400

    added 9-16

---
+++ Example14/Program.cs
@@ -0,0 +1,17 @@
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Clear();
+Console.Write($"Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if (num%7 == 0 && num%23 == 0)
+{
+    Console.WriteLine("да");
+}else
+{
+    Console.WriteLine("нет");
+}
+
---
+++ Example16/Program.cs
@@ -0,0 +1,21 @@
+﻿// 16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+
+Console.Clear();
+Console.Write($"Введите 1-ое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine($"да");
+}
+else
+{
+    Console.WriteLine($"нет");
+}
